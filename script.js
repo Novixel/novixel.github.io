8a7313d4d0f5cb640d86a3f3f30578c95fc776f3
Novixel <Novixel@hotmail.com>
2023-12-18 21:35:56 -0700

    0.0.3

---
+++ script.js
@@ -0,0 +1,35 @@
+var darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+var darkModeSwitch = document.getElementById('darkModeSwitch');
+
+// Set initial switch position based on system preference
+if (darkModeMediaQuery.matches) {
+    document.body.classList.add('dark-mode');
+    darkModeSwitch.firstChild.className = 'fas fa-sun';
+} else {
+    document.body.classList.remove('dark-mode');
+    darkModeSwitch.firstChild.className = 'fas fa-moon';
+}
+function toggleDarkMode(event) {
+    var button = event.target;
+    var isDarkMode = document.body.classList.toggle('dark-mode');
+    if (isDarkMode) {
+        button.className = 'fas fa-sun';
+    } else {
+        button.className = 'fas fa-moon';
+    }
+}
+document.getElementById('darkModeSwitch').addEventListener('click', toggleDarkMode);
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
