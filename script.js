eccf56dc2cbe69ef9c3d13d61a7c256a992582c1
Novixel <Novixel@hotmail.com>
2024-12-08 11:05:55 -0700

    0.0.6
    Small Updates

--- script.js
+++ script.js
@@ -25,6 +25,14 @@ function toggleDarkMode(event) {
 }
 document.getElementById('darkModeSwitch').addEventListener('click', toggleDarkMode);
 
+let counter = 0;
+
+function incrementCounter(event) {
+    
+    counter++;
+    document.getElementById('counterLabel').textContent = counter.toString();
+}
+document.getElementById('counterLabel').addEventListener('click', incrementCounter);
 
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
